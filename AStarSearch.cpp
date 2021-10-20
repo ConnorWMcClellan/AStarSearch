8356d2f500ce81a48470ee73056ac9ee911a3fce
ConnorWMcClellan <cwm200@lindenwood.edu>
2021-10-19 20:05:12 -0500

    10/19/21 Commit, Outline of project

--- AStarSearch.cpp
+++ AStarSearch.cpp
@@ -2,7 +2,20 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char const *argv[])
 {
-	
+	/*
+	1. Create txt file containig each city and the distance between
+	two cities.
+
+	2. Create a graph using the above text file.
+
+	3. Take in user input of a starting city and a destination city(Handle
+	any error for user input error such as a city not contained in the map(graph))
+
+	4. Implement A* star search.
+
+	5. Return the optimal route between inputted cities.
+	 */
+	return 0;
 }
